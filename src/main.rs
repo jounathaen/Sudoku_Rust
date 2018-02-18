778e65862cc00ff0ed411120f98d46e73a4e2486
Jonathan Christoph Klimt <jonathan.klimt@rwth-aachen.de>
2018-02-18 22:19:15 +0100

    use csv reader for sudokus

--- src/main.rs
+++ src/main.rs
@@ -24,19 +24,9 @@ fn solve_sudokus(file_path: &String) -> Result<(), Box<Error>> {
     Ok(())
 }
 
-    // Init
-    let mut sud: Sudoku = Default::default();
-    sud.print(false);
-    sudoku_easy_1::fill_sudoku(&mut sud);
-    sud.check_validity();
-    sud.print(false);
-    // sud.print(true);
-    let mut count = 0;
-    while {sud.solve_obvious() > 0 } {
-        count = count + 1;
-        println!("Round {}", count);
-        sud.print(false);
-    }
 
-    sud.check_validity();
+    let file_path = "/";
+    if let Err(err) = solve_sudokus(&String::from(file_path)) {
+        println!("{}", err);
+    }
 }
