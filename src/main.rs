1d309e4056f3ec6809b1726cb9b7c76fd1968c2f
Jonathan Christoph Klimt <jonathan.klimt@rwth-aachen.de>
2018-02-18 22:14:50 +0100

    Added csv reader to massively solve sudokus

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
+extern crate csv;
+
+use std::fs::File;
+use std::error::Error;
+
 mod field;
 use field::*;
 
-mod sudoku_easy_1;
 
 
+// mod sudoku_easy_1;
 
-fn main() {
+fn solve_sudokus(file_path: &String) -> Result<(), Box<Error>> {
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+            let mut sud: Sudoku = Default::default();
+            if let Some(substring) = result.unwrap().get(0){
+                println!("{:?}", substring);
+                sud.read_from_string(&String::from(substring));
+                sud.easy_solve();
+            }
+    }
+    Ok(())
+}
 
     // Init
     let mut sud: Sudoku = Default::default();
