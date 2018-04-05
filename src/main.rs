0fa3fbda539c06ad1c012fd51ddb136961eb1478
Jonathan <jonathan.klimt@rwth-aachen.de>
2018-04-05 13:35:50 +0200

    Cleaned up main

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,37 @@
-extern crate csv;
-
-use std::fs::File;
-use std::error::Error;
+// use std::error::Error;
 
 mod field;
 use field::*;
 
 
-
-// mod sudoku_easy_1;
-
-fn solve_sudokus(file_path: &String) -> Result<(), Box<Error>> {
-    let file = File::open(file_path)?;
-    let mut rdr = csv::Reader::from_reader(file);
-    for result in rdr.records() {
-            let mut sud: Sudoku = Default::default();
-            if let Some(substring) = result.unwrap().get(0){
-                println!("{:?}", substring);
-                sud.read_from_string(&String::from(substring));
-                sud.easy_solve();
-            }
-    }
-    Ok(())
-}
-
-
 fn main() {
-    let file_path = "/";
-    if let Err(err) = solve_sudokus(&String::from(file_path)) {
+    // Solve Sudokus from csv file
+    let file_path = "data/sudoku_small.csv";
+    if let Err(err) = solve_sudokus_from_csv(&String::from(file_path)) {
         println!("{}", err);
     }
+
+    // Solve Sudokus from String
+    let mut sud: Sudoku;
+    sud = Sudoku::from(String::from("904008010060092007001040802243900080090000206006030700000405000009000068705610304"));
+
+    // sud.print_lvl = Lvl::Verbose;
+    // sud.print_lvl = Lvl::Interactive;
+    sud.print_lvl = Lvl::Solution;
+
+    sud.easy_solve().unwrap();
+
+    // Solution:
+    // 924 578 613
+    // 368 192 457
+    // 571 346 892
+
+    // 243 967 185
+    // 197 854 236
+    // 856 231 749
+
+    // 632 485 971
+    // 419 723 568
+    // 785 619 324
+
 }
